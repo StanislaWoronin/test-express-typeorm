0a3cfef279082a4780dcc12ef7b3866b6f425b92
Stanislav <stanislaworonin@yandex.ru>
2023-06-05 15:15:53 +0300

    remove postgres connection

--- src/settings.ts
+++ src/settings.ts
@@ -3,4 +3,9 @@ dotenv.config();
 
 export const settings = {
 	port: process.env.PORT,
+	postgres: {
+		username: process.env.POSTGRESS_NAME,
+		password: process.env.POSTGRESS_PASSWORD,
+		database: process.env.POSTGRESS_DATABASE,
+	},
 };
